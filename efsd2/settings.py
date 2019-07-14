63e95042ee14b05ecf711799cfc187dc552dc604
wlcobb <42555289+wlcobb@users.noreply.github.com>
2019-07-14 08:41:42 -0500

    Update settings.py

--- efsd2/settings.py
+++ efsd2/settings.py
@@ -165,8 +165,8 @@ STATICFILES_DIRS = (
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 LOGIN_REDIRECT_URL = '/home'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
